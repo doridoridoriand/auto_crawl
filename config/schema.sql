42d5d1b837fab63f49f1997ffb817f121e445d2f
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-09-21 18:12:23 +0900

    [WIP] creating auto page parser

--- config/schema.sql
+++ config/schema.sql
@@ -15,9 +15,10 @@ create table `target_web_sites` (
 ) engine=InnoDB default charset=utf8;
 
 create table `target_links` (
-  `id`        bigint(40)   unsigned not null,
-  `top_url`   varchar(768) not null,
-  `url`       varchar(768) not null,
-  `page_type` varchar(256),
+  `id`           bigint(40)   unsigned not null,
+  `top_url`      varchar(768) not null,
+  `relative_url` varchar(768) not null,
+  `absolute_url` varchar(768),
+  `page_type`    varchar(256),
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
--- lib/db/target_link.rb
+++ lib/db/target_link.rb
@@ -1,2 +1,13 @@
 class TargetLink < ActiveRecord::Base
 end
+
+module TargetLinkModule
+  # absolute_urlとpage_typeに関しては存在しない場合も考慮しないといけない
+  def sasve_items(id = 0)
+    TargetLink.create(
+      id: id,
+      top_url: self[:top_url],
+      relative_url: self[:relative_url],
+    )
+  end
+end
