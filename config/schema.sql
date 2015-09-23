8215565fabbb8c350e3485c86d4942e95f6fd368
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-09-23 15:11:58 +0900

    change database column size

--- config/schema.sql
+++ config/schema.sql
@@ -4,7 +4,7 @@ create table `crawled_origin_pages` (
   `top_url`   varchar(768)  not null,
   `page_type` varchar(256)  not null,
   `url`       varchar(768)  not null,
-  `html`      varchar(2048) not null,
+  `html`      mediumtext not null,
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
 
